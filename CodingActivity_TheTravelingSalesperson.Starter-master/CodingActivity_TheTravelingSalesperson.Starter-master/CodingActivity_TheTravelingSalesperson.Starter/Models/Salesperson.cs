656e0a425f6bfea7cee957c9b82667b2ea900424
kadyvre <mitch04@live.com>
2016-09-15 10:55:30 -0400

    Add files via upload

---
+++ CodingActivity_TheTravelingSalesperson.Starter-master/CodingActivity_TheTravelingSalesperson.Starter-master/CodingActivity_TheTravelingSalesperson.Starter/Models/Salesperson.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingActivity_TheTravelingSalesperson
+{
+    public class Salesperson
+    {
+        #region FIELDS
+
+        private string _firstName;
+        private string _lastName;
+        #endregion
+        
+        #region PROPERTIES
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        #endregion
+
+
+        #region CONSTRUCTORS
+
+        //public Salesperson()
+        //{
+
+        //}
+
+        public Salesperson(string firstName, string lastName)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+        }
+
+        #endregion
+
+
+        #region METHODS
+
+
+
+        #endregion
+    }
+}
